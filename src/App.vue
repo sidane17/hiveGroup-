c5c960537ea349608c8958879539f4b17f28da47
sidane17 <khalilsidane89@gmail.com>
2024-10-22 17:05:14 +0200

    sezione uno

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,64 @@ export default{
 
 <template>
   <Navbar/>
-  <div class="s1 w-100"> </div>
+  
+    <div class="sezione_uno d-flex justify-content-start align-items-center "> 
+      <img src="./assets/Esagoni.png" alt="" width="30%">
+      <div class="container d-flex justify-content-center align-items-center ">
+         <div class="titol col-7 d-flex justify-content-center align-items-center flex-column">
+          <h2 id="font"><b>Conetti.</b></h2>
+          <h2 id="font"><b>Crea.</b></h2>
+          <h2 id="font"><b>Sorprendi.</b></h2>
+         </div>
+        <div class="titol  col-5">
+          <div> </div>
+        </div>
+      </div>
+   
+    </div>
+    <div class="sezione_due ">
+
+    </div>
+ 
+  
 </template>
 
-<style scoped>
-.tutto{
-  height: 100vh;
+<style >
 
+.sezione_uno{
+  width: 100%;
+  height: 55vh;
+  position: relative;
+  z-index: 0;
+  overflow-y: hidden;
+  background: linear-gradient(to bottom, white, antiquewhite); /* Gradiente dal bianco all'antique white */
+  overflow: hidden;
+   border-style: dashed;
+
+}
+.im{
+  position: relative;
+}
+.titol{
+  border-style: dashed;
+  height: 30rem;
+}
+#font{
+  font-size: 6rem;
+  color: rgb(226, 148, 30);
+}
+.sezione_due{
+  width: 100%;
+  height: 100vh;
+  border-style: dashed;
+  background-color: antiquewhite;
+}
+.sezione_uno > img {
+  position: absolute;
+ top: 20px;
+ right: -100px;
+  transform: rotate(40deg); /* Ruota di 45 gradi */
+  transition: transform 0.5s; /* Transizione fluida */
+  z-index: 99;
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <nav class="navbar navbar-expand-lg ">
+    <nav class="navbar navbar-expand-lg sticky-top   ">
         <div class="container d-flex justify-content-between">
             <div class="col-1">
             <img src="../assets/HIVE-Group_Logo.png" alt="" width="100%">
@@ -13,9 +13,10 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarScroll">
                 <div class="d-flex justify-content-end flex-grow-1">
-                    <a href="#" class="nav-link me-3">SERVIZI</a>
-                    <a href="#" class="nav-link me-3">PARTNER</a>
-                    <a href="#" class="nav-link me-3">CONTATTACI</a>
+                    <a href="#" class="nav-link me-3"><strong>SERVIZI</strong></a>
+                    <a href="#" class="nav-link me-3"><strong>PARTNER</strong></a>
+                    <a href="#" class="nav-link me-3"><strong>CONTATTACI</strong></a>
+                    <a href="#" class="nav-link me-3"><strong>CHI SIAMO </strong></a>
                 </div>
             </div>
         </div>
@@ -24,8 +25,19 @@
 
 
 <style scoped>
+a{
+    color: rgb(210, 117, 30);
+}
 a:hover{
-    color: chocolate;
+    color: rgb(210, 168, 30);
     
+}
+nav{
+    position: sticky;
+    z-index: -99;
+    
+}
+nav{
+    height: 10vh;
 }
 </style>
